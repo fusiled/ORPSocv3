f1bec0c98dd6380cc736e2fe984811e2cfd3f59d
Matteo Fusi <fusiled@gmail.com>
2016-09-17 22:39:00 +0200

    improved wb_snoop_arbiter comments

--- src/wb_intercon-1.0/rtl/verilog/wb_snoop_arbiter.v
+++ src/wb_intercon-1.0/rtl/verilog/wb_snoop_arbiter.v
@@ -1,6 +1,41 @@
-//TODO write do nothing with other cores
-//TODO fix code using ise
+/* ****************************************************************************
+  This Source Code Form is subject to the terms of the
+  Open Hardware Description License, v. 1.0. If a copy
+  of the OHDL was not distributed with this file, You
+  can obtain one at http://juliusbaxter.net/ohdl/ohdl.txt
 
+  Description: bus arbiter that implement a simple snoop protocol
+
+  parameters tells how connected buses are organized. 
+  - num_master is THE TOTAL NUMBER of buses connected to the arbiter 
+    (instruction, data, debug unit... everithing that must access the memory)
+  - num_dbus is the number of buses on which we apply the snoop protocol. CONNECT
+    THE BUSES AT THE RIGHT SIDE OF THE INPUT, otherwise the protocol won't work. 
+    The snoop protocol will active only on buses [num_dbus-1:0]
+  
+  The snoop protocol actives only on memory reads and on the range [num_dbus-1:0]
+  swithcing the state from IDLE to SNOOP_READ:
+    1) The arbiter blocks the access to memory
+    2) It asserts the output snoop_type to signal cores that a snoop event is 
+         taking place
+    3) It sends the address to snoop to the other dbuses through the snoop_adr_o
+    4) It waits for core responses checking snoop_ack_i and snoop_hit_i:
+      - snoop_ack_i tells the arbiter that cores are replying to the snoop arbiter
+      - snoop_hit_i tells the arbiter if a core has the requested datum.
+    5) if snoop_ack_i = {num_dbus{1'b1}} and snoop_hit_i = 0 then the poll is failed
+          got to MEM_ACCESS state and let the memory satisfy the request
+      ELSE
+       forward snooped datum (that is saved in snooped_dat register) to the core
+    6) The transaction ends when end_of_transaction is asserted. Now the state is
+       switched to IDLE.
+
+
+
+  Copyright (C) 2016 Authors
+
+  Author(s): Matteo M. Fusi <fusiled@gmail.com>
+
+***************************************************************************** */
 
 module wb_snoop_arbiter
  #(parameter dw = 32,
@@ -62,14 +97,19 @@ module wb_snoop_arbiter
    wire [master_sel_bits-1:0] master_sel;
    wire active;
 
+   //signal if the snoop poll had success or not. See POLL_RESPONSE_x params.
+   //The default value is POLL_RESPONSE_UNDEFINED. It can assume
+   //POLL_RESPONSE_POSITIVE or POLL_RESPONSE_NEGATIVE only after we know for
+   //sure the poll result and only when the state==IDLE
    reg [1:0] poll_response_flag;
 
+   //This register proxies the wbm_cyc_i in the arbiter module. The idea is to
+   //"trick" the arbiter as much as possible cycle, but also exploiting its
+   //arbitration abilities.
    reg [num_masters-1:0] arbiter_cyc;
    reg [aw-1:0] snoop_adr_reg;
-   reg mem_access_active;
-   
-   reg [3:0] state;
-   reg [3:0] next_state;
+
+   //Snoop params
    localparam IDLE = 4'b0001;
    localparam SNOOP_WRITE = 4'b0010;
    localparam SNOOP_READ = 4'b0100;
@@ -78,16 +118,20 @@ module wb_snoop_arbiter
    localparam SNOOP_TYPE_IDLE = 1'b0;
    localparam SNOOP_TYPE_READ = 1'b1;
 
+   //used by poll_response_flag
    localparam POLL_RESPONSE_UNDEFINED = 2'b10;
    localparam POLL_RESPONSE_POSITIVE = 2'b11;
    localparam POLL_RESPONSE_NEGATIVE = 2'b00;
    
    
-   reg [num_dbus-1:0] poll_result;
-   reg [num_dbus-1:0]snoop_datum_owner;
+   reg [3:0] state;
+   //buffer register. It is needed to update the state
+   reg [3:0] next_state;
+
    reg [master_sel_bits-1:0] saved_master_sel;
-   reg snoop_read_ready;
    wire end_of_transaction;
+
+   //patch to simplify the poll_response assignment
    wire snoop_mask;
    wire snoop_type;
 
@@ -103,8 +147,7 @@ module wb_snoop_arbiter
 	  
    
 
-//TODO snoop in progress
-   //Mux active master
+   //output signals
    assign wbs_adr_o = wbm_adr_i[master_sel*aw+:aw];
    assign wbs_dat_o = wbm_dat_i[master_sel*dw+:dw];
    assign wbs_sel_o = wbm_sel_i[master_sel*4+:4];
@@ -125,27 +168,24 @@ module wb_snoop_arbiter
                               (wbs_ack_i & active) << saved_master_sel :
                               (state == IDLE && active==1 && wbm_we_i[master_sel]==0 && master_sel < num_dbus) ? 0 :
                               (wbs_ack_i & active) << master_sel;
-   /*assign wbm_ack_o = state == IDLE ?
-                      ((wbs_ack_i & active) << master_sel) :
-                        state == SNOOP_READ ?
-                           1<<saved_master_sel :
-                           0;*/
+
    assign wbm_err_o = ((wbs_err_i & active) << master_sel);
    assign wbm_rty_o = ((wbs_rty_i & active) << master_sel);
 
-   assign snoop_type = (state == SNOOP_READ ) ? SNOOP_TYPE_READ : SNOOP_TYPE_IDLE ;
    assign snoop_type_o = snoop_type;
+
+   assign snoop_adr_o = {num_dbus{snoop_adr_reg}};
 
 
    assign end_of_transaction = ( (state == SNOOP_READ && wbm_cyc_i[saved_master_sel]==0) ||
                                  (state == MEM_ACCESS && wbm_cyc_i[master_sel]==0) ) ? 1 : 0;
 
-
-   assign snoop_adr_o = {num_dbus{snoop_adr_reg}};
+   assign snoop_type = (state == SNOOP_READ ) ? SNOOP_TYPE_READ : SNOOP_TYPE_IDLE ;
 
    assign snoop_mask = 1<<saved_master_sel;
 
 
+   //FSM that controls next_state variable and then the state register.
    always@(posedge wb_clk_i)
    begin
       //reset condition
@@ -153,7 +193,7 @@ module wb_snoop_arbiter
       begin
          next_state = IDLE;
       end
-      state = next_state;
+      saved_master_sel = 0;
       case(state)
          IDLE:
          begin
@@ -162,6 +202,7 @@ module wb_snoop_arbiter
             begin
               //$display("switch to SNOOP_READ due to active:%b, wbm_we_i:%b, master_sel:%d, num_dbus:%d, wbm_cyc_i: %b", active, wbm_we_i, master_sel, num_dbus, wbm_cyc_i);
                next_state = SNOOP_READ;
+               saved_master_sel = master_sel;
             end
          end
          SNOOP_READ:
@@ -187,41 +228,41 @@ module wb_snoop_arbiter
          default:
          begin
             next_state = IDLE;
+            saved_master_sel = 0;
          end
       endcase
    end
 
    always@(*)
    begin: fsm_sensitivity_block
-      arbiter_cyc = wbm_cyc_i;
-      snoop_adr_reg = 0;
-      saved_master_sel = 0;
+      //don't interfere with memory access
+      arbiter_cyc <= wbm_cyc_i;
+      snoop_adr_reg <= 0;
+      //unless we change state!
+      state <= next_state;
       case(state)
          IDLE: 
          begin
-            arbiter_cyc = wbm_cyc_i;
-            snoop_adr_reg = 0;
-            saved_master_sel = 0;
+            //don't interfere
+            arbiter_cyc <= wbm_cyc_i;
+            snoop_adr_reg <= 0;
          end
          SNOOP_READ:
          begin
             //block arbitration, save the selected core and the current request array
-            arbiter_cyc = 0;
-            snoop_adr_reg = wbm_adr_i[master_sel*aw+:aw];
-            saved_master_sel = master_sel;
+            arbiter_cyc <= 0;
+            snoop_adr_reg <= wbm_adr_i[master_sel*aw+:aw];
          end
          MEM_ACCESS: 
          begin
             //restore state before snoop read and access to memory
-            arbiter_cyc = wbm_cyc_i;
-            snoop_adr_reg = 0;
-            saved_master_sel = master_sel;
+            arbiter_cyc <= wbm_cyc_i;
+            snoop_adr_reg <= 0;
          end
          default: 
          begin
-            arbiter_cyc = wbm_cyc_i;
-            snoop_adr_reg = 0;
-            saved_master_sel = 0;
+            arbiter_cyc <= wbm_cyc_i;
+            snoop_adr_reg <= 0;
          end
       endcase
    end
